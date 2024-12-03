88ece7fc6e24210f136e64d590386ed4a6e181d4
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-12-03 21:32:32 +0530

    Create Cart.js

---
+++ ICA01/src/components/Cart.js
@@ -0,0 +1,33 @@
+export default function Cart({cart}) {
+    const grandTotal = cart.reduce((total, item) => total + item.totalPrice, 0);
+    return (
+        <div className="table-container">
+            <h4 className="card-title">Cart</h4>
+            <table>
+                <thead>
+
+                    <td>Product</td>
+                    <td>QTY</td>
+                    <td>Price</td>
+
+                </thead>
+                <tbody>
+                {
+                 cart.map(item=>(
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.qty}</td>
+                        </tr>
+                    )) 
+                }
+                </tbody>
+                <tr>
+                    <td >Grand Total : </td>
+                    <td colSpan={2}>${grandTotal}</td>
+                </tr>
+            </table>
+           
+        </div>
+    );
+}
